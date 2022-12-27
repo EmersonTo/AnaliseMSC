df961bfee93b78d4149dd1e1d90fc984684abfa5
unknown <emerson.certa.solutions@gmail.com>
2022-12-27 15:15:16 -0300

    comite inicial

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.title("Análise do Arquivo MSC")
 st.sidebar.header("Cidades")
 
 uploaded_files = st.file_uploader(
-    "Choose a CSV file", accept_multiple_files=True)
+    "Escolha o arquivo MSC", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+    st.write("Nome Arquivo:", uploaded_file.name)
     st.write(bytes_data)
