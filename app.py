d33864d6819dd2086c9dbcd027e2ca2a07274f55
unknown <emerson.certa.solutions@gmail.com>
2022-12-27 16:09:11 -0300

    comite inicial

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ st.sidebar.header("Cidades")
 uploaded_file = st.file_uploader("Escolha o arquivo MSC",  type=['csv'])
 if uploaded_file is not None:
     detalhes = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
-    dataframe = pd.read_csv(uploaded_file)
+    dataframe = pd.read_csv(uploaded_file, encoding='utf-8',sep=';')
     st.dataframe(dataframe)
+
 
     
