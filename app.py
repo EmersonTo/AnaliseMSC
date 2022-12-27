4ce2f2cf5ea526d10f9e4f86fcfce432fa2849fb
unknown <emerson.certa.solutions@gmail.com>
2022-12-27 15:58:25 -0300

    comite inicial

--- app.py
+++ app.py
@@ -8,9 +8,22 @@ import funcoes
 st.title("Análise do Arquivo MSC")
 st.sidebar.header("Cidades")
 
-uploaded_file = st.file_uploader("Escolha o arquivo MSC", accept_multiple_files=False, type=['csv'])
-bytes_data = uploaded_file.read()
-st.write("Nome Arquivo:", uploaded_file.name)
-df = pd.read_csv(uploaded_file)
-st.dataframe(df)
+uploaded_file = st.file_uploader("Escolha o arquivo MSC",  type=['csv'])
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
     
