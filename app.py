56655c8d7408a7cce0c7e946ddeb260566385567
unknown <emerson.certa.solutions@gmail.com>
2022-12-27 15:54:25 -0300

    upload de arquivo

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ import funcoes
 st.title("Análise do Arquivo MSC")
 st.sidebar.header("Cidades")
 
-uploaded_files = st.file_uploader(
-    "Escolha o arquivo MSC", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("Nome Arquivo:", uploaded_file.name)
-    funcoes.importa_base_dados(uploaded_file)
+uploaded_file = st.file_uploader("Escolha o arquivo MSC", accept_multiple_files=False, type=['csv'])
+bytes_data = uploaded_file.read()
+st.write("Nome Arquivo:", uploaded_file.name)
+df = pd.read_csv(uploaded_file)
+st.dataframe(df)
     
